d11012942f93ad039f9505b496a7a59fef23ea07
yousifalansari <y.alansari2006@gmail.com>
2025-10-18 02:59:12 +0300

    css adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
-// src/App.jsx
+import WeatherForecast from "./Components/WeatherForecast/WeatherForecast";
 
-const App = () => {
+const weatherForecasts = [
+  {
+    day: "Mon",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg",
+    imgAlt: "sun icon",
+    conditions: "sunny",
+    time: "Morning",
+  },
+  {
+    day: "Tue",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg",
+    imgAlt: "moon icon",
+    conditions: "clear",
+    time: "Night",
+  },
+  {
+    day: "Wed",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg",
+    imgAlt: "clouds with lightning icon",
+    conditions: "stormy",
+    time: "All Day",
+  },
+  {
+    day: "Thu",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg",
+    imgAlt: "sun overcast by clouds icon",
+    conditions: "overcast",
+    time: "Evening",
+  },
+  {
+    day: "Fri",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg",
+    imgAlt: "moon overcast by clouds icon",
+    conditions: "cloudy",
+    time: "Night",
+  },
+];
 
+export default function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Local Weather</h1>
+      <section>
+        {weatherForecasts.map((forecast, index) => (
+          <WeatherForecast
+            key={index}
+            day={forecast.day}
+            img={forecast.img}
+            imgAlt={forecast.imgAlt}
+            conditions={forecast.conditions}
+            time={forecast.time}
+          />
+        ))}
+      </section>
+    </>
   );
 }
-
-export default App
---
+++ src/Components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import "./WeatherForecast.css";
+
+function WeatherForecast({ day, img, imgAlt, conditions, time }) {
+  return (
+    <div className="weather">
+      <h2>{day}</h2>
+      <img src={img} alt={imgAlt} />
+      <p><span>conditions: </span>{conditions}</p>
+      <p><span>time: </span>{time}</p>
+    </div>
+  );
+}
+
+export default WeatherForecast;
